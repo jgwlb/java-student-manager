e34d18b6a5094041f5b6bda7986498143e90b531
sl <3365957838@qq.com>
2026-08-30 15:34:23 +0800

    refactor: 学生管理改用HashMap 存储

--- StudentSystem.java
+++ StudentSystem.java
@@ -1,9 +1,10 @@
 package newa;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 public class StudentSystem {
-    private ArrayList<Student> studentList = new ArrayList();
+    private HashMap<String,Student> studentMap = new HashMap<>();
     private Scanner sc;
 
     public StudentSystem() {
@@ -40,7 +41,7 @@ public class StudentSystem {
     private void addStudent() {
         System.out.println("请输入学号：");
         String id = this.sc.next();
-        if (this.findStudentIndex(id) != -1) {
+        if (this.studentMap.containsKey(id)) {
             System.out.println("学号已存在，添加失败");
         } else {
             System.out.println("请输入姓名：");
@@ -50,7 +51,7 @@ public class StudentSystem {
             System.out.println("请输入性别：");
             String gender = this.sc.next();
             Student student = new Student(id, name, age, gender);
-            this.studentList.add(student);
+            this.studentMap.put(id,student);
             System.out.println("添加完成");
         }
     }
@@ -58,11 +59,10 @@ public class StudentSystem {
     private void deleteStudent() {
         System.out.println("请输入要删除的学号：");
         String id = this.sc.next();
-        int index = this.findStudentIndex(id);
-        if (index == -1) {
+        if (!this.studentMap.containsKey(id)) {
             System.out.println("学号不存在，删除失败");
         } else {
-            this.studentList.remove(index);
+            this.studentMap.remove(id);
             System.out.println("删除成功");
         }
     }
@@ -70,8 +70,7 @@ public class StudentSystem {
     private void updateStudent() {
         System.out.println("请输入要修改的学号：");
         String id = this.sc.next();
-        int index = this.findStudentIndex(id);
-        if (index == -1) {
+        if (!this.studentMap.containsKey(id)) {
             System.out.println("学号不存在，修改失败");
         } else {
             System.out.println("请输入新姓名：");
@@ -81,29 +80,21 @@ public class StudentSystem {
             System.out.println("请输入新性别：");
             String gender = this.sc.next();
             Student student = new Student(id, name, age, gender);
-            this.studentList.set(index, student);
+            this.studentMap.put(id, student);
             System.out.println("修改完成");
         }
     }
 
     private void queryStudent() {
-        if (this.studentList.isEmpty()) {
+        if (this.studentMap.isEmpty()) {
             System.out.println("暂无学生信息");
         } else {
-            for(Student student : this.studentList) {
+            for(Student student : this.studentMap.values()) {
                 System.out.println(student);
             }
 
         }
     }
 
-    private int findStudentIndex(String id) {
-        for(int i = 0; i < this.studentList.size(); ++i) {
-            if (((Student)this.studentList.get(i)).getId().equals(id)) {
-                return i;
-            }
-        }
 
-        return -1;
-    }
 }
