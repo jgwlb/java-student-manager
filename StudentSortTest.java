98cf2915903e81169442310d62ea2f23f26b4d6e
sl <3365957838@qq.com>
2026-09-07 15:09:41 +0800

    feat: Comparable与Comparator排序实验

--- StudentSortTest.java
+++ StudentSortTest.java
@@ -21,13 +21,18 @@ import java.util.TreeMap;
 public class StudentSortTest {
 
     // 内部类:一个人,有名字和年龄
-    static class Person {
+    static class Person implements Comparable<Person> {
+
         String name;
         int age;
 
         Person(String name, int age) {
             this.name = name;
             this.age = age;
+        }
+        @Override
+        public int compareTo(Person o){
+            return this.age - o.age;
         }
 
         @Override
@@ -37,12 +42,15 @@ public class StudentSortTest {
     }
 
     public static void main(String[] args) {
-        TreeMap<Person, String> map = new TreeMap<>();
+        TreeMap<Person, String> byAge = new TreeMap<>((a,b)->a.age-b.age);
+        TreeMap<Person, String> byName = new TreeMap<>((a, b) -> a.name.compareTo(b.name));
+        TreeMap<Person, String> byAgeDesc = new TreeMap<>((a, b) -> b.age - a.age);
+        byAge.put(new Person("张三", 20), "a");
+        byAge.put(new Person("李四", 25), "b");
+        byAge.put(new Person("王五", 18), "c");
 
-        map.put(new Person("张三", 20), "a");
-        map.put(new Person("李四", 25), "b");
-        map.put(new Person("王五", 18), "c");
-
-        System.out.println(map);
+        System.out.println("按年龄   : " + byAge.keySet());
+        System.out.println("按姓名   : " + byName.keySet());
+        System.out.println("按年龄倒序: " + byAgeDesc.keySet());
     }
 }
